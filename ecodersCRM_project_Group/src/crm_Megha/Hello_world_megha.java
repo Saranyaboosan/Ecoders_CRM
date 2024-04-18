960c3bfb56fbc94dabd6b13891e8399fbc581c2d
gurupreethsingh <igurupreeth@gmail.com>
2024-04-18 17:57:03 +0530

    edited megha's hello world code in linke 7

--- ecodersCRM_project_Group/src/crm_Megha/Hello_world_megha.java
+++ ecodersCRM_project_Group/src/crm_Megha/Hello_world_megha.java
@@ -4,9 +4,6 @@ public class Hello_world_megha {
 
 	public static void main(String[] args)
 		{
-		System.out.println("hello world");
-
+		System.out.println("hello world from megha");
+	    }
 	}
-	}
-
-
