1ded90340d8f09b3f3f1975018540dfe9e1b178a
saranyaboosan <saranya.pgp2009@gmail.com>
2024-04-22 11:03:18 +0530

    crm project doc updated

--- Ecoders_CRM/src/smoke_Scenarios/OpenHomepage.java
+++ Ecoders_CRM/src/smoke_Scenarios/OpenHomepage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenHomepage {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
     {
 		//System.setProperty("webdriver.chorme.driver", "chromedriver.exe");
 		// open the chrome broswer. 
@@ -18,34 +18,7 @@ public class OpenHomepage {
 //		driver.navitage().to("http://localhost:5173");
 		
 		Thread.sleep(2000);
-		
-		
-		// to capture the url, or the title of the page. 
-		String actualtitle = driver.getTitle();
-		String actulurl = driver .getCurrentUrl();
-		
-		String expectedtittle = "";
-		
-		String expectedurl = "";
-		
-		if(expectedtittle.equals(actualtitle))
-		{
-			System.out.println("Test case pass, title matches.");
-		}
-		else {
-			System.out.println("Test case failed, title did not matches.");
-		}
-		
-		if(expectedurl.equals(actulurl))
-		{
-			System.out.println("Test case pass, url matches.");
-		}
-		else {
-			System.out.println("Test case failed, url did not matches.");
-		}
-		
-		Thread.sleep(4000);
-		
+			
 		driver.quit();
 
 	}
